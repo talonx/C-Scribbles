86fd7f9a22411e361c9338da7c8518b03d9d4f3f
Hrishikesh Barua <talonx@gmail.com>
2011-01-30 00:10:07 +0530

    Removed hardcoded port

--- net/mynet.c
+++ net/mynet.c
@@ -95,7 +95,7 @@ void startserver(void (*func)(int), int port) {
 	bzero(&servaddr, sizeof(servaddr));
 	servaddr.sin_family = AF_INET;
 	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-	servaddr.sin_port = htons(1025);
+	servaddr.sin_port = htons(port);
 	
 	int bres = bind(listenfd, (struct sockaddr *) &servaddr, sizeof(servaddr));
 	handleerr(bres);
